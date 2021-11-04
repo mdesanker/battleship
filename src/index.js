749005be293372ecfac7e7b10fc4c159ceda4ddf
Michael Desanker <mdesanker@gmail.com>
2021-11-04 19:10:14 -0400

    feat: started gameplay functionality, but need to develop some UI

--- src/index.js
+++ src/index.js
@@ -5,16 +5,42 @@ import { Ship } from "./modules/ship";
 import { Gameboard } from "./modules/gameboard";
 import { Player } from "./modules/player";
 
-// Battleship game loop
-let isGameOver = false;
-while (!isGameOver) {
-  // Create player objects
-  const player = Player("Human");
-  const computer = Player("Computer");
+const playRound = (playerBoard, computerBoard) => {
   let round = 0;
 
   // Alternate player with each round
-  let currentPlayer = round % 2 === 0 ? player : computer;
+  // let currentPlayer = round % 2 === 0 ? player : computer;
+
+  // Player attack computer board
+  const coord = window.prompt("Choose coordinates to attack");
+  computerBoard.receiveAttack(coord);
+
+  // Check computer has ships remaining
+  if (computerBoard.checkAllShipsSunk()) {
+    console.log("Computer has no ships remaining");
+  }
+
+  // Computer attack player board
+  playerBoard.randomAttack();
+
+  // Check player has ships remaining
+  if (playerBoard.checkAllShipsSunk()) {
+    console.log("Computer has no ships remaining");
+  }
+
+  // Increment round counter
+  round++;
+};
+
+// Battleship game loop
+let roundTest = 0;
+let isGameOver = false;
+while (!isGameOver) {
+  // while (roundTest <= 10) {
+  // Create player objects
+  const player = Player("Human");
+  const computer = Player("Computer");
+
   // console.log(currentPlayer.getName());
 
   // Create board objects
@@ -22,6 +48,9 @@ while (!isGameOver) {
   const computerBoard = Gameboard();
 
   playerBoard.placeShipsRandom(player.getShips());
+  computerBoard.placeShipsRandom(computer.getShips());
+
+  // playRound(playerBoard, computerBoard);
 
   // console.log(player.getShip("carrier").getPosition());
 
@@ -29,7 +58,8 @@ while (!isGameOver) {
     // console.log(ship.getPosition());
   });
 
-  playerBoard.randomAttack();
+  // playerBoard.randomAttack();
 
   isGameOver = true;
+  roundTest++;
 }
