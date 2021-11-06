24833da6a9fe19fd2776ed9a5f27c106420291c1
Michael Desanker <mdesanker@gmail.com>
2021-11-06 07:03:30 -0400

    feat: add checkbox to allow placing ships vertically

--- src/index.js
+++ src/index.js
@@ -30,29 +30,39 @@ const DisplayController = (() => {
   // Place player ships manually
   const placementBoard = document.querySelector(".placement-board");
   const shipDetails = document.querySelector(".ship-details");
+  const dirToggle = document.querySelector("#direction");
   console.log(shipDetails.textContent);
 
   const shipsRemaining = player.getShips();
 
   let i = 0;
-  shipDetails.textContent = `${player.getShips()[i].getName()} [${player
+  shipDetails.textContent = `Click to place ${player
     .getShips()
-    [i].getLength()}]`;
+    [i].getName()} [${player.getShips()[i].getLength()}]`;
+
   placementBoard.addEventListener("click", (e) => {
     const clicked = [
       Number.parseInt(e.target.dataset.col),
       Number.parseInt(e.target.dataset.row),
     ];
+    console.log(dirToggle.checked);
+    console.log(dirToggle.value);
     if (i < player.getShips().length) {
       // Place next ship in list on player board
-      playerBoard.placeShip(player.getShips()[i], "h", clicked);
+      playerBoard.placeShip(
+        player.getShips()[i],
+        dirToggle.checked ? "v" : "h",
+        clicked
+      );
       displayPlayerShips(player);
       // Verify ship successfully placed on board
-      if (playerBoard.shipsOnBoard() === i + 1) {
+      if (playerBoard.shipsOnBoard() === i + 1 && i < 4) {
         i++;
-        shipDetails.textContent = `${player.getShips()[i].getName()} [${player
+        shipDetails.textContent = `Click to place ${player
           .getShips()
-          [i].getLength()}]`;
+          [i].getName()} [${player.getShips()[i].getLength()}]`;
+      } else if (i === 4) {
+        shipDetails.textContent = "All ships placed";
       }
     }
   });
